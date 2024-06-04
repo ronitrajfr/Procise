1e44506c36f67b2138734b16352320b800ee48ac
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-05 00:39:03 +0530

    feat: og + next config

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,21 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const mainog = "/public/og.jpg"
 
 export const metadata: Metadata = {
   title: "Procise",
   description: "Summarize Hacker News Comments",
+  openGraph: {
+    images: [mainog],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Procise",
+    description: "Summarize Hacker News Comments",
+    images: [mainog],
+    creator: "@ronitraj_",
+  },
 };
 
 export default function RootLayout({
